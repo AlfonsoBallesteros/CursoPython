c44694a4ad1aaf5b0a18f18907b450903ff09e18
Alfonso ballessteros <ponchob1258@gmail.com>
2019-08-24 12:11:50 -0500

    tipo de datos

---
+++ tipo_datos.py
@@ -0,0 +1,21 @@
+#type --> funcion para concoer el tipo de dato
+print(type(2.0))#Float
+
+print(type('palabra'))#String
+
+print(type(False))#booleano
+
+print(type(10 == 10))#booleano
+
+print(type(3 + 5)) #Int
+
+print(type([5,7,65,67])) #list
+
+print(type((10,25,68))) #Tupla
+
+print(type({   #Diccionario
+    "name":"alfonso",
+    "lastname": "Ballesteros",
+    "nickname":"elAlfonso",
+    "celphone":31717526
+}))
