638b0dc6f6978b71a7662d97a596b2d0d049ea35
Alfonso ballessteros <ponchob1258@gmail.com>
2019-08-24 13:10:08 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+print('hola ' + 'hola') #concatenar
+
+print('programdor ' *3)
+
+#manejo de variables
+pi = 3.1416
+radio = 3
+area = pi * radio **2
+
+print(area)
+
+print('')
+print('------------------------------')
+print('')
+
+print('cual es tu nombre') 
+name = str(input()) #entrada de texto
+print('Hola ' + name + '!')
