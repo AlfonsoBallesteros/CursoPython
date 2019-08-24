aae2d09f1501b4f985f135e2f154dd054bd0fc2e
Alfonso ballessteros <ponchob1258@gmail.com>
2019-08-24 11:45:17 -0500

    mi primer commit

---
+++ operadores.py
@@ -0,0 +1,20 @@
+print("operadores")
+s = 2 + 2 #suma
+print (s)
+r = 5 - 6 #resta
+print (r)
+m = 5 * 8 #multiplicacion
+print (m)
+d = 20 / 5 
+print (d)
+
+M = 9 % 2 #Moduluo
+print (M)
+
+p = 3**2 #Potencia
+print(p)
+#Boleeano
+b = 3 > 5 
+b1 = 5 < 10
+b2 = 10 == 10
+print(b,b1,b2)
